9a11c86b6bd1d9ebef3c0f5554fae12b627d6c43
Andrew Cowie <andrew@operationaldynamics.com>
2020-02-22 14:20:52 +1100

    Minimal working example that demonstrates problem
    This builds against `lts-15.0` with GHC 8.8.2; unfortunately **ghcide**
    segfaults when checking it for errors. Demonstrates
    https://github.com/digital-asset/ghcide/issues/444 as requested by
    @mpickering whose help we hugely appreciate.

--- src/HelloMain.hs
+++ src/HelloMain.hs
@@ -1,10 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
 
 module Main where
 
 import Core.Program
 
+version :: Version
+version = $(fromPackage)
+
 main :: IO ()
-main = execute $ do
+main = do
+    context <- configure version None blank
+    executeWith context program
+
+program :: Program None ()
+program = do
     write "Hello World"
